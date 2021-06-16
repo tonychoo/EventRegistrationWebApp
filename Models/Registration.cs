5dbb3a625abe1152bf127cb1a6424b56c826b7cc
Tony Choo <coolax@gmail.com>
2021-06-17 00:07:04 +0800

    Update add registration and add in validation.

--- Models/Registration.cs
+++ Models/Registration.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
@@ -10,20 +11,31 @@ namespace EventRegistrationWebApp.Models
         [DatabaseGeneratedAttribute(DatabaseGeneratedOption.Identity)]
         public int ID { get; set; }
 
+        [MaxLength(30)]
         [DataType(DataType.Text)]
         public string Name { get; set; }
+
+        [MaxLength(50)]
+        [RegularExpression("^[a-zA-Z0-9_\\.-]+@([a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,6}$", ErrorMessage = "Email entered is not valid")]
         [DataType(DataType.Text)]
         public string Email { get; set; }
 
+        [MaxLength(1)]
         [DataType(DataType.Text)]
         public string Gender { get; set; }
-
+        
+        [DisplayName("Date Registered")]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:dd/MM/yyyy}")]
+        [Range(typeof(DateTime), "1/1/2019", "30/6/2019", ErrorMessage = "Allowed date is between {1} to {2}")]
         [DataType(DataType.Date)]
         public DateTime RegistrationDate { get; set; }
 
+        [DisplayName("Selected Days")]
         [NotMapped]
         public string[] EventDays { get; set; }
 
+        [DisplayName("Additional Request")]
+        [MaxLength(100)]
         public string AdditionalRequest { get; set; }
     }
 }
